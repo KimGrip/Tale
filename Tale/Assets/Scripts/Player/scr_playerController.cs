9c0fd8929f688790e516e95b3d30242fc59ff0db
Kim Gripenberg <kim.grip@hotmail.com>
2016-03-29 15:52:00 +0200

    By Kim: Added Camera Controller, Added Basic Movement

--- Tale/Assets/Scripts/Player/scr_playerController.cs
+++ Tale/Assets/Scripts/Player/scr_playerController.cs
@@ -6,19 +6,63 @@ public enum PlayerState
 };
 public class scr_playerController : MonoBehaviour {
 
-    public float m_MovementSpeed;
+    public float m_inputDelay;
+    public float m_ForwardSpeed;
     public float m_RotationSpeed;
-    public bool m_Grounded;
 
+    Quaternion m_targetRotation;
+    Rigidbody m_rb;
+
+    float forwardInput, turnInput;
+
+    public Quaternion GetTargetRotation
+    {
+        get { return m_targetRotation; }
+    }
 	// Use this for initialization
 	void Start () 
     {
-	
+        m_targetRotation = transform.rotation;
+        if (GetComponent<Rigidbody>())
+        {
+            m_rb = GetComponent<Rigidbody>();
+        }
+        else
+        {
+            Debug.LogError("The player needs a rigidbody");
+        }
+        forwardInput = 0; turnInput = 0;
 	}
-	
+    void GetInput()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horizontal");
+    }
+
 	// Update is called once per frame
 	void Update () 
     {
-	
+        GetInput();
+        Turn();
 	}
+    void FixedUpdate()
+    {
+        Run();
+    }
+    void Run()
+    {
+        if(Mathf.Abs(forwardInput) > m_inputDelay)
+        {
+            m_rb.velocity = transform.forward * forwardInput * m_ForwardSpeed;
+        }
+        else
+        {
+            m_rb.velocity = Vector3.zero;
+        }
+    }
+    void Turn()
+    {
+        m_targetRotation *= Quaternion.AngleAxis(m_RotationSpeed * turnInput * Time.deltaTime,Vector3.up  );
+        transform.rotation = m_targetRotation;
+    }
 }
