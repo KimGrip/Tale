966f4888a7992ed9ac8d26a5c1b491b0e301dac1
Kim Gripenberg <kim.grip@hotmail.com>
2016-03-29 12:27:02 +0200

    By Kim: Added functionality, materials, map structure.

---
+++ Tale/Assets/Scripts/Player/scr_playerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+public enum PlayerState
+{
+    Idle, Running, Jumping, Climbing, Aiming, 
+};
+public class scr_playerController : MonoBehaviour {
+
+    public float m_MovementSpeed;
+    public float m_RotationSpeed;
+    public bool m_Grounded;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
